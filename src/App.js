7cb29355765da870ad996c8742a86eb1b1c2528a
Mahammad Mostafa <mahammad.mostafa@hotmail.com>
2023-08-10 03:25:09 +0300

    Imported & rendered calculator component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import Calculator from './components/Calculator';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Welcome to ReactJS</p>
-      </header>
-    </div>
-  );
+  return <Calculator />;
 }
 
 export default App;
