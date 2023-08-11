ff0ecf685e65804014dfa8de0d0d3b42328b6191
Mahammad Mostafa <mahammad.mostafa@hotmail.com>
2023-08-11 04:24:19 +0300

    Implemented event & state handling

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,23 +1,42 @@
+import React from 'react';
 import PropTypes from 'prop-types';
+import Calculate from '../logic/calculate';
 import Styles from '../styles/calculator.module.css';
 
-const Button = ({ name }) => {
-  if (['/', '*', '-', '+', '='].includes(name)) {
-    return <button type="button" className={`${Styles.button} ${Styles.orange}`}>{name}</button>;
+const Button = ({ name, handler }) => {
+  let className = `${Styles.button}`;
+  if (['÷', 'x', '-', '+', '='].includes(name)) {
+    className = `${Styles.button} ${Styles.orange}`;
+  } else if (name === '0') {
+    className = `${Styles.button} ${Styles.zero}`;
   }
-  return <button type="button" className={name === '0' ? Styles.zero : Styles.button}>{name}</button>;
+  return <button type="button" onClick={handler} className={className}>{name}</button>;
 };
 
 Button.propTypes = { name: PropTypes.string.isRequired };
+Button.propTypes = { handler: PropTypes.func.isRequired };
 
-const Calculator = () => {
-  const values = ['AC', '+/-', '%', '/', '7', '8', '9', '*', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
-  return (
-    <section className={Styles.section}>
-      <p className={Styles.result}>0</p>
-      {values.map((value) => <Button key={value} name={value} />)}
-    </section>
-  );
-};
+export default class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { operation: { } };
+    this.handleButton = this.handleButton.bind(this);
+    this.values = ['AC', '+/-', '%', '÷', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
+  }
 
-export default Calculator;
+  handleButton = (event) => {
+    this.setState((state) => ({ operation: Calculate(state.operation, event.target.textContent) }));
+  }
+
+  displayButtons = (value) => <Button key={value} name={value} handler={this.handleButton} />;
+
+  render = () => {
+    const { operation: { total, next } } = this.state;
+    return (
+      <section className={Styles.section}>
+        <p className={Styles.result}>{next === null || next === undefined ? (total || 0) : next}</p>
+        {this.values.map(this.displayButtons)}
+      </section>
+    );
+  }
+}
