68d15d44d1f1300513a8315606b9f79258112d30
Mahammad Mostafa <mahammad.mostafa@hotmail.com>
2023-08-20 04:38:01 +0300

    Added routes components for all pages with layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import Quote from './components/quote';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Layout from './components/layout';
+import Home from './components/home';
 import Calculator from './components/calculator';
+import Quote from './components/quote';
 
 function App() {
   return (
-    <>
-      <Quote />
-      <Calculator />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="calculator" element={<Calculator />} />
+          <Route path="quote" element={<Quote />} />
+          <Route path="*" element={<h2>404 Not Found</h2>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
