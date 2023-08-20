b0833076759948c0cfeb2f69c4d2fea90563f622
Mahammad Mostafa <mahammad.mostafa@hotmail.com>
2023-08-20 04:43:05 +0300

    Implemented simple fixed footer

---
+++ src/components/footer.js
@@ -0,0 +1,9 @@
+import Styles from '../styles/footer.module.css';
+
+const Footer = () => (
+  <footer className={Styles.footer}>
+    <p className={Styles.text}>&copy; 2023 All rights reserved</p>
+  </footer>
+);
+
+export default Footer;
